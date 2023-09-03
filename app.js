87fa060828a3d54bd95e2155ae3f9b4874254f6a
chente31209 <chente31209@hotmail.com>
2023-09-02 20:46:13 -0600

    se agrego le prototipo conseptaual

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ let app = express()
 
 app.set("port", process.env.PORT || 3000 )
 
-app.get('/',(req,res)=>{
-    return res.send("Hola Mundo")
-})
+app.use("/", express.static('public'));
 
 app.listen(app.get('port'),()=>{
     console.log("http://127.0.0.1:"+app.get('port')+"");
